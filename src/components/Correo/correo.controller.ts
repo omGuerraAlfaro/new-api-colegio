fb35664a8d847f138146fa6cd68db086e81cd5d7
Omar Guerra Alfaro <omarrrrignacio@gmail.com>
2023-11-07 21:11:01 -0300

    Update correo.controller.ts

--- src/components/Correo/correo.controller.ts
+++ src/components/Correo/correo.controller.ts
@@ -56,4 +56,51 @@ export class CorreoController {
       throw new Error('Hubo un problema al enviar el correo. Por favor, intenta nuevamente.');
     }
   }
+  @Post('/enviar/verano')
+  @UsePipes(new ValidationPipe())
+  async enviarCorreoVerano(@Body() formData: any): Promise<any> {
+    try {
+      // Obteniendo credenciales de las variables de entorno
+      const emailUser = this.configService.get<string>('EMAIL_USER');
+      const emailPass = this.configService.get<string>('EMAIL_PASS');
+
+      // Configuración del transporte de correo usando Nodemailer
+      const transporter = nodemailer.createTransport({
+        host: 'mail.colegioandeschile.cl',
+        port: 465,
+        secure: true, 
+        auth: {
+          user: emailUser,
+          pass: emailPass
+        }
+      });
+
+      // Construye el cuerpo del correo con los datos del formulario
+      const correoHtml = `
+        <p><strong>Nombre completo del postulante:</strong> ${formData.pupilo}</p>
+        <p><strong>Nombre completo del apoderado:</strong> ${formData.apoderado}</p>
+        <p><strong>Curso al que postula:</strong> ${formData.direccion}</p>
+        <p><strong>Teléfono:</strong> ${formData.telefono}</p>
+        <p><strong>Email:</strong> ${formData.email}</p>
+        <p><strong>Consentimiento:</strong> ${formData.consentimiento ? 'Sí' : 'No'}</p>
+      `;
+
+      // Opciones del correo
+      const mailOptions = {
+        from: emailUser,
+        to: 'omar.guerra@outlook.cl',
+        subject: 'Postulación Colegio Andes de Chile',
+        html: correoHtml,
+      };
+
+      // Envía el correo
+      const result = await transporter.sendMail(mailOptions);
+
+      // Devuelve una respuesta de éxito
+      return { message: 'Correo enviado con éxito', result };
+    } catch (error) {
+      console.error('Error al enviar el correo:', error);
+      throw new Error('Hubo un problema al enviar el correo. Por favor, intenta nuevamente.');
+    }
+  }
 }
