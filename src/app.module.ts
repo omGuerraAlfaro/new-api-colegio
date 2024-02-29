7fa3be42f2b383867d7044649e0fc5b78fa33a94
Omar Guerra Alfaro <omar.guerra@valposystems.cl>
2024-02-28 23:35:38 -0300

    prueba

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,9 +21,9 @@ import { ApoderadoModule, EstudianteModule, ProfesorModule, CursoModule, Usuario
       type: 'mysql',
       host: '44.207.168.60',
       port: 3306,
-      username: 'colegioa_omarignacio',
-      password: '068gJOWTJ@@k',
-      database: 'colegioa_colegioandeschile',
+      username: 'colegioa_omarignacio2',
+      password: 'E(@dS&^r61YM',
+      database: 'colegioa_colegioandeschile2',
       entities,
       synchronize: true
     }),
