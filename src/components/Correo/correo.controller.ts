c3807c72d3347c47a96f902219eec5f59a209e95
Omar Guerra Alfaro <omar.guerra@valposystems.cl>
2024-03-21 19:32:03 -0300

    correo talleres

--- src/components/Correo/correo.controller.ts
+++ src/components/Correo/correo.controller.ts
@@ -116,4 +116,53 @@ export class CorreoController {
       throw new Error('Hubo un problema al enviar el correo. Por favor, intenta nuevamente.');
     }
   }
+
+  @Post('/enviar/taller')
+  @UsePipes(new ValidationPipe())
+  async enviarCorreoTaller(@Body() formData: any): Promise<any> {
+    try {
+      const smtpInfo = await this.correoSmtp.getCorreoSmtp();
+      const { host_smtp, port_smtp, user_smtp, pass_smtp } = smtpInfo[0];
+
+      // Configuración del transporte de correo usando Nodemailer
+      const transporter = nodemailer.createTransport({
+        host: host_smtp,
+        port: port_smtp,
+        secure: true,
+        auth: {
+          user: user_smtp,
+          pass: pass_smtp
+        }
+      });
+
+      // Construye el cuerpo del correo con los datos del formulario
+      const correoHtml = `
+        <h4><strong>Formulario inscripción Talleres 2024</strong></h4>
+        <p><strong>Nombre completo del alumno/a:</strong> ${formData.pupilo}</p>
+        <p><strong>Curso al que postula:</strong> ${formData.cursoPostula}</p>
+        <p><strong>Taller al que postula:</strong> ${formData.tallerPostula}</p>
+        <p><strong>Nombre completo del apoderado:</strong> ${formData.apoderado}</p>
+        <p><strong>Teléfono:</strong> ${formData.telefono}</p>
+        <p><strong>Email:</strong> ${formData.email}</p>
+        <p><strong>Consentimiento:</strong> ${formData.consentimiento ? 'Sí' : 'No'}</p>
+      `;
+
+      // Opciones del correo
+      const mailOptions = {
+        from: user_smtp,
+        to: 'omar.guerra@outlook.cl',
+        subject: 'Inscripción Talleres 2024 Colegio Andes de Chile',
+        html: correoHtml,
+      };
+
+      // Envía el correo
+      const result = await transporter.sendMail(mailOptions);
+
+      // Devuelve una respuesta de éxito
+      return { message: 'Correo enviado con éxito', result };
+    } catch (error) {
+      console.error('Error al enviar el correo:', error);
+      throw new Error('Hubo un problema al enviar el correo. Por favor, intenta nuevamente.');
+    }
+  }
 }
