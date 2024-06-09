e482e52baa01b98585847e6b692606b4adcf7256
Omar Guerra Alfaro <102171656+omGuerraAlfaro@users.noreply.github.com>
2024-06-09 19:13:44 -0400

    login?

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,33 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from 'filters/all-exception.filter';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Pipes y filtros globales
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
+
   app.enableCors({
-    origin: ['https://www.colegioandeschile.cl', 'https://www.intranet.colegioandeschile.cl', 'http://localhost', 'http://localhost:8200', 'http://localhost:8100', 'http://localhost:4200', 'http://localhost:8101', '*'],
+    origin: [
+      'https://www.colegioandeschile.cl',
+      'https://www.intranet.colegioandeschile.cl',
+      'http://localhost',
+      'http://localhost:8200',
+      'http://localhost:8100',
+      'http://localhost:4200',
+      'http://localhost:8101',
+      '*'
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
-    optionsSuccessStatus: 204, // 204 es típicamente usado para respuestas preflight exitosas
-    // allowedHeaders: 'Content-Type, Accept, Authorization',
-    allowedHeaders: ['Content-Type', 'Tbk-Api-Key-Id', 'Tbk-Api-Key-Secret'],
-    credentials: true, // si necesitas enviar/recibir cookies o autenticación basada en encabezados
+    optionsSuccessStatus: 204,
+    allowedHeaders: [
+      'Content-Type',
+      'Tbk-Api-Key-Id',
+      'Tbk-Api-Key-Secret',
+    ],
+    credentials: false,
   });
 
   const config = new DocumentBuilder()
@@ -26,6 +40,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
   await app.listen(3200);
 }
+
 bootstrap();
