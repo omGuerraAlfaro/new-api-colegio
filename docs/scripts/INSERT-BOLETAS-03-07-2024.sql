77ab3599b7376ee2354a298542ef7636eea1041c
Omar Guerra Alfaro <102171656+omGuerraAlfaro@users.noreply.github.com>
2024-07-03 18:32:37 -0400

    insert alumnos new

---
+++ docs/scripts/INSERT-BOLETAS-03-07-2024.sql
@@ -0,0 +1,110 @@
+-- boletas Benjamin Orostica 6A OKKKKKKKKKKKKKKKK
+INSERT INTO boleta (id, apoderado_id, pago_id, estado_id, detalle, subtotal, iva, total, descuento, nota, fecha_vencimiento, rut_estudiante, rut_apoderado) VALUES
+(2124, 508, NULL, 2, 'Boleta de matricula', 220000.00, 19.00, 220000.00, 220000.00, 'Boleta mes de matricula', '2024-01-01 00:00:00', '24067958', '17164267'),
+(2125, 508, NULL, 2, 'Boleta de marzo', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de marzo', '2024-03-01 00:00:00', '24067958', '17164267'),
+(2126, 508, NULL, 2, 'Boleta de abril', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de abril', '2024-04-01 00:00:00', '24067958', '17164267'),
+(2127, 508, NULL, 2, 'Boleta de mayo', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de mayo', '2024-05-01 00:00:00', '24067958', '17164267'),
+(2128, 508, NULL, 1, 'Boleta de junio', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de junio', '2024-06-01 00:00:00', '24067958', '17164267'),
+(2129, 508, NULL, 1, 'Boleta de julio', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de julio', '2024-07-01 00:00:00', '24067958', '17164267'),
+(2130, 508, NULL, 1, 'Boleta de agosto', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de agosto', '2024-08-01 00:00:00', '24067958', '17164267'),
+(2131, 508, NULL, 1, 'Boleta de septiembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de septiembre', '2024-09-01 00:00:00', '24067958', '17164267'),
+(2132, 508, NULL, 1, 'Boleta de octubre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de octubre', '2024-10-01 00:00:00', '24067958', '17164267'),
+(2133, 508, NULL, 1, 'Boleta de noviembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de noviembre', '2024-11-01 00:00:00', '24067958', '17164267'),
+(2134, 508, NULL, 1, 'Boleta de diciembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de diciembre', '2024-12-01 00:00:00', '24067958', '17164267');
+
+-- boletas Julian Rivera 6A OKKKKKKKKKKKKKK
+
+INSERT INTO boleta (id, apoderado_id, pago_id, estado_id, detalle, subtotal, iva, total, descuento, nota, fecha_vencimiento, rut_estudiante, rut_apoderado) VALUES
+(2135, 509, NULL, 2, 'Boleta de matricula', 220000.00, 19.00, 220000.00, 220000.00, 'Boleta mes de matricula', '2024-01-01 00:00:00', '24268005', '16381171'),
+(2136, 509, NULL, 2, 'Boleta de marzo', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de marzo', '2024-03-01 00:00:00', '24268005', '16381171'),
+(2137, 509, NULL, 2, 'Boleta de abril', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de abril', '2024-04-01 00:00:00', '24268005', '16381171'),
+(2138, 509, NULL, 2, 'Boleta de mayo', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de mayo', '2024-05-01 00:00:00', '24268005', '16381171'),
+(2139, 509, NULL, 1, 'Boleta de junio', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de junio', '2024-06-01 00:00:00', '24268005', '16381171'),
+(2140, 509, NULL, 1, 'Boleta de julio', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de julio', '2024-07-01 00:00:00', '24268005', '16381171'),
+(2141, 509, NULL, 1, 'Boleta de agosto', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de agosto', '2024-08-01 00:00:00', '24268005', '16381171'),
+(2142, 509, NULL, 1, 'Boleta de septiembre', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de septiembre', '2024-09-01 00:00:00', '24268005', '16381171'),
+(2143, 509, NULL, 1, 'Boleta de octubre', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de octubre', '2024-10-01 00:00:00', '24268005', '16381171'),
+(2144, 509, NULL, 1, 'Boleta de noviembre', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de noviembre', '2024-11-01 00:00:00', '24268005', '16381171'),
+(2145, 509, NULL, 1, 'Boleta de diciembre', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de diciembre', '2024-12-01 00:00:00', '24268005', '16381171');
+
+
+-- boletas Fernando Rivera 7A falta id apoderado
+
+INSERT INTO boleta (id, apoderado_id, pago_id, estado_id, detalle, subtotal, iva, total, descuento, nota, fecha_vencimiento, rut_estudiante, rut_apoderado) VALUES
+(2146, 509, NULL, 2, 'Boleta de matricula', 220000.00, 19.00, 220000.00, 220000.00, 'Boleta mes de matricula', '2024-01-01 00:00:00', '23761995', '16381171'),
+(2147, 509, NULL, 2, 'Boleta de marzo', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de marzo', '2024-03-01 00:00:00', '23761995', '16381171'),
+(2148, 509, NULL, 2, 'Boleta de abril', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de abril', '2024-04-01 00:00:00', '23761995', '16381171'),
+(2149, 509, NULL, 2, 'Boleta de mayo', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de mayo', '2024-05-01 00:00:00', '23761995', '16381171'),
+(2150, 509, NULL, 1, 'Boleta de junio', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de junio', '2024-06-01 00:00:00', '23761995', '16381171'),
+(2151, 509, NULL, 1, 'Boleta de julio', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de julio', '2024-07-01 00:00:00', '23761995', '16381171'),
+(2152, 509, NULL, 1, 'Boleta de agosto', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de agosto', '2024-08-01 00:00:00', '23761995', '16381171'),
+(2153, 509, NULL, 1, 'Boleta de septiembre', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de septiembre', '2024-09-01 00:00:00', '23761995', '16381171'),
+(2154, 509, NULL, 1, 'Boleta de octubre', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de octubre', '2024-10-01 00:00:00', '23761995', '16381171'),
+(2155, 509, NULL, 1, 'Boleta de noviembre', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de noviembre', '2024-11-01 00:00:00', '23761995', '16381171'),
+(2156, 509, NULL, 1, 'Boleta de diciembre', 165000.00, 19.00, 165000.00, 165000.00, 'Boleta mes de diciembre', '2024-12-01 00:00:00', '23761995', '16381171');
+
+
+
+-- boletas Agustin Leiva 8A OKKKKKKKKKKKK
+
+INSERT INTO boleta (id, apoderado_id, pago_id, estado_id, detalle, subtotal, iva, total, descuento, nota, fecha_vencimiento, rut_estudiante, rut_apoderado) VALUES
+(2157, 123, NULL, 2, 'Boleta de matricula', 220000.00, 19.00, 220000.00, 220000.00, 'Boleta mes de matricula', '2024-01-01 00:00:00', '23761995', '13225494'),
+(2158, 123, NULL, 2, 'Boleta de marzo', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de marzo', '2024-03-01 00:00:00', '23761995', '13225494'),
+(2159, 123, NULL, 2, 'Boleta de abril', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de abril', '2024-04-01 00:00:00', '23761995', '13225494'),
+(2160, 123, NULL, 2, 'Boleta de mayo', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de mayo', '2024-05-01 00:00:00', '23761995', '13225494'),
+(2161, 123, NULL, 1, 'Boleta de junio', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de junio', '2024-06-01 00:00:00', '23761995', '13225494'),
+(2162, 123, NULL, 1, 'Boleta de julio', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de julio', '2024-07-01 00:00:00', '23761995', '13225494'),
+(2163, 123, NULL, 1, 'Boleta de agosto', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de agosto', '2024-08-01 00:00:00', '23761995', '13225494'),
+(2164, 123, NULL, 1, 'Boleta de septiembre', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de septiembre', '2024-09-01 00:00:00', '23761995', '13225494'),
+(2165, 123, NULL, 1, 'Boleta de octubre', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de octubre', '2024-10-01 00:00:00', '23761995', '13225494'),
+(2166, 123, NULL, 1, 'Boleta de noviembre', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de noviembre', '2024-11-01 00:00:00', '23761995', '13225494'),
+(2167, 123, NULL, 1, 'Boleta de diciembre', 293750.00, 19.00, 293750.00, 293750.00, 'Boleta mes de diciembre', '2024-12-01 00:00:00', '23761995', '13225494');
+
+
+
+-- boletas Emilia Gomez 6A OKKKKKKKKKKKKK
+
+INSERT INTO boleta (id, apoderado_id, pago_id, estado_id, detalle, subtotal, iva, total, descuento, nota, fecha_vencimiento, rut_estudiante, rut_apoderado) VALUES
+(2168, 510, NULL, 2, 'Boleta de matricula', 220000.00, 19.00, 220000.00, 220000.00, 'Boleta mes de matricula', '2024-01-01 00:00:00', '24016770', '16501896'),
+(2169, 510, NULL, 2, 'Boleta de marzo', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de marzo', '2024-03-01 00:00:00', '24016770', '16501896'),
+(2170, 510, NULL, 2, 'Boleta de abril', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de abril', '2024-04-01 00:00:00', '24016770', '16501896'),
+(2171, 510, NULL, 2, 'Boleta de mayo', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de mayo', '2024-05-01 00:00:00', '24016770', '16501896'),
+(2172, 510, NULL, 1, 'Boleta de junio', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de junio', '2024-06-01 00:00:00', '24016770', '16501896'),
+(2173, 510, NULL, 1, 'Boleta de julio', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de julio', '2024-07-01 00:00:00', '24016770', '16501896'),
+(2174, 510, NULL, 1, 'Boleta de agosto', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de agosto', '2024-08-01 00:00:00', '24016770', '16501896'),
+(2175, 510, NULL, 1, 'Boleta de septiembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de septiembre', '2024-09-01 00:00:00', '24016770', '16501896'),
+(2176, 510, NULL, 1, 'Boleta de octubre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de octubre', '2024-10-01 00:00:00', '24016770', '16501896'),
+(2177, 510, NULL, 1, 'Boleta de noviembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de noviembre', '2024-11-01 00:00:00', '24016770', '16501896'),
+(2178, 510, NULL, 1, 'Boleta de diciembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de diciembre', '2024-12-01 00:00:00', '24016770', '16501896');
+
+
+-- boletas Matheo Meza 7A OKKKKKKKKKKKKKK
+
+INSERT INTO boleta (id, apoderado_id, pago_id, estado_id, detalle, subtotal, iva, total, descuento, nota, fecha_vencimiento, rut_estudiante, rut_apoderado) VALUES
+(2179, 511, NULL, 2, 'Boleta de matricula', 220000.00, 19.00, 220000.00, 220000.00, 'Boleta mes de matricula', '2024-01-01 00:00:00', '23703701', '13469506'),
+(2180, 511, NULL, 2, 'Boleta de marzo', 195000.00, 19.00, 0, 195000.00, 'Boleta mes de marzo', '2024-03-01 00:00:00', '23703701', '13469506'),
+(2181, 511, NULL, 2, 'Boleta de abril', 195000.00, 19.00, 0, 195000.00, 'Boleta mes de abril', '2024-04-01 00:00:00', '23703701', '13469506'),
+(2182, 511, NULL, 2, 'Boleta de mayo', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de mayo', '2024-05-01 00:00:00', '23703701', '13469506'),
+(2183, 511, NULL, 1, 'Boleta de junio', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de junio', '2024-06-01 00:00:00', '23703701', '13469506'),
+(2184, 511, NULL, 1, 'Boleta de julio', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de julio', '2024-07-01 00:00:00', '23703701', '13469506'),
+(2185, 511, NULL, 1, 'Boleta de agosto', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de agosto', '2024-08-01 00:00:00', '23703701', '13469506'),
+(2186, 511, NULL, 1, 'Boleta de septiembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de septiembre', '2024-09-01 00:00:00', '23703701', '13469506'),
+(2187, 511, NULL, 1, 'Boleta de octubre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de octubre', '2024-10-01 00:00:00', '23703701', '13469506'),
+(2188, 511, NULL, 1, 'Boleta de noviembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de noviembre', '2024-11-01 00:00:00', '23703701', '13469506'),
+(2189, 511, NULL, 1, 'Boleta de diciembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de diciembre', '2024-12-01 00:00:00', '23703701', '13469506');
+
+
+-- boletas Paz Peña 7A falta id apoderado
+
+INSERT INTO boleta (id, apoderado_id, pago_id, estado_id, detalle, subtotal, iva, total, descuento, nota, fecha_vencimiento, rut_estudiante, rut_apoderado) VALUES
+(2190, 512, NULL, 2, 'Boleta de matricula', 220000.00, 19.00, 220000.00, 220000.00, 'Boleta mes de matricula', '2024-01-01 00:00:00', '23891898', '13006301'),
+(2191, 512, NULL, 2, 'Boleta de marzo', 195000.00, 19.00, 0, 195000.00, 'Boleta mes de marzo', '2024-03-01 00:00:00', '23891898', '13006301'),
+(2192, 512, NULL, 2, 'Boleta de abril', 195000.00, 19.00, 0, 195000.00, 'Boleta mes de abril', '2024-04-01 00:00:00', '23891898', '13006301'),
+(2193, 512, NULL, 2, 'Boleta de mayo', 195000.00, 19.00, 0, 195000.00, 'Boleta mes de mayo', '2024-05-01 00:00:00', '23891898', '13006301'),
+(2194, 512, NULL, 1, 'Boleta de junio', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de junio', '2024-06-01 00:00:00', '23891898', '13006301'),
+(2195, 512, NULL, 1, 'Boleta de julio', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de julio', '2024-07-01 00:00:00', '23891898', '13006301'),
+(2196, 512, NULL, 1, 'Boleta de agosto', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de agosto', '2024-08-01 00:00:00', '23891898', '13006301'),
+(2197, 512, NULL, 1, 'Boleta de septiembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de septiembre', '2024-09-01 00:00:00', '23891898', '13006301'),
+(2198, 512, NULL, 1, 'Boleta de octubre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de octubre', '2024-10-01 00:00:00', '23891898', '13006301'),
+(2199, 512, NULL, 1, 'Boleta de noviembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de noviembre', '2024-11-01 00:00:00', '23891898', '13006301'),
+(2200, 512, NULL, 1, 'Boleta de diciembre', 195000.00, 19.00, 195000.00, 195000.00, 'Boleta mes de diciembre', '2024-12-01 00:00:00', '23891898', '13006301');
